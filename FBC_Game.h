e99d4c246976f69e16564df83c193163c5d6c134
Austin <austin.greene.1346@gmail.com>
2019-01-05 18:14:21 -0600

    Create FBC_Game.h

---
+++ FBC_Game.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <string>
+
+using FString = std::string;
+using int32 = int;
+
+struct FBullCowCount {
+	int32 Bulls = 0;
+	int32 Cows = 0;
+};
+
+enum class EGuessStatus {
+	Invalid_Status,
+	OK,
+	Not_Isogram,
+	Wrong_Length,
+	Not_Lowercase
+};
+
+class FBC_Game {
+public:
+	FBC_Game(); //Constructor
+
+	int32 GetMaxTries() const;
+	int32 GetCurrentTry() const;
+	int32 GetHiddenWordLength() const;
+
+	bool IsGameWon() const;
+	EGuessStatus CheckGuessValidity(FString) const;
+
+	void Reset();
+
+	//Counts bulls and cows, and increases try number assuming valid guess.
+	FBullCowCount SubmitGuess(FString);
+
+private:
+	//See constructor for init
+	int32 MyCurrentTry;
+	int32 MyMaxTries;
+	FString MyHiddenWord;
+};
